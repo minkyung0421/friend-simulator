58b18817154be054f2b3fcf8ddee1697c975ec05
minkyoung choi <aza923@e-mirim.hs.kr>
2017-04-06 20:16:33 +0900

    #8 첫번째 버튼 동작하게 하기 - onClick 메소드

--- app/src/main/java/kr/hs/emirim/aza923/friendsimulator/choihyerim/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/aza923/friendsimulator/choihyerim/MainActivity.java
@@ -2,6 +2,9 @@ package kr.hs.emirim.aza923.friendsimulator.choihyerim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    voic onClick(Viwe view){
+        Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
     }
 }
