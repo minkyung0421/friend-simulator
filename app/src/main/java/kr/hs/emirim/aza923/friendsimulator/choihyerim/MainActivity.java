bc274b1f91a9fce778eb8a7eaf28d01aba2b6af3
minkyoung choi <aza923@e-mirim.hs.kr>
2017-04-06 20:35:07 +0900

    #9 버튼 여러개를 onClick에서 모두 처리하게 하기

--- app/src/main/java/kr/hs/emirim/aza923/friendsimulator/choihyerim/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/aza923/friendsimulator/choihyerim/MainActivity.java
@@ -1,10 +1,9 @@
 package kr.hs.emirim.aza923.friendsimulator.choihyerim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +13,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    voic onClick(Viwe view){
-        Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+    void onClick(View view){
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(this, "냐~", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button2:
+                Toast.makeText(this, "신곡 나온데", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button3:
+                Toast.makeText(this, "탄산", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button4:
+                Toast.makeText(this, "배고파", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button5:
+                Toast.makeText(this, "안돼안돼", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "뭐라고 하는지 모르겠어요", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
